532b940ef2ecc8f6e43a0e6eb60c25b9c78acc3f
Erik Sundell <erik.sundell87@gmail.com>
2018-11-26 15:50:54 +0100

    Testing the test

--- clubService.js
+++ clubService.js
@@ -1,27 +1,31 @@
-const clubs = require('./clubs.json')
+const clubs = require('./clubs.json');
 
-const clubService = {}
+const clubService = {};
 
 clubService.getAllClubs = () => {
-  let result = []
-  clubs.endpoints.matchi.forEach((club) => {
-    result.push(Object.assign({}, club, { tag: 'matchi' }))
-  })
+  let result = [];
+  clubs.endpoints.matchi.forEach(club => {
+    result.push(Object.assign({}, club, { tag: 'matchi' }));
+  });
 
-  clubs.endpoints.matchiPadel.forEach((club) => {
-    result.push(Object.assign({}, club, { tag: 'matchipadel' }))
-  })
+  clubs.endpoints.matchiPadel.forEach(club => {
+    result.push(Object.assign({}, club, { tag: 'matchipadel' }));
+  });
 
-  clubs.endpoints.myCourt.clubs.forEach((club) => {
-    result.push(Object.assign({}, club, { tag: 'mycourt', url: club.bookingUrl }))
-  })
+  clubs.endpoints.myCourt.clubs.forEach(club => {
+    result.push(
+      Object.assign({}, club, { tag: 'mycourt', url: club.bookingUrl })
+    );
+  });
 
-  result.push(Object.assign({}, clubs.endpoints.hellas, { tag: 'hellas' }))
+  result.push(Object.assign({}, clubs.endpoints.hellas, { tag: 'hellas' }));
 
-  let enskede = clubs.endpoints.enskede
-  result.push(Object.assign({}, enskede, { tag: 'enskede', url: enskede.bookingUrl }))
+  let enskede = clubs.endpoints.enskede;
+  result.push(
+    Object.assign({}, enskede, { tag: 'enskede', url: enskede.bookingUrl })
+  );
 
-  return result
-}
+  return result;
+};
 
-module.exports = clubService
+module.exports = clubService;
